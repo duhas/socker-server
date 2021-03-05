be1e5ecafcaeaf360b83c80e00dd5ce2f4f452e2
duhas <fabian@codigosdelsur.cl>
2021-03-05 09:35:42 -0300

    seccion 4

---
+++ sockets/sockets.ts
@@ -0,0 +1,17 @@
+import SocketIO from "socket.io";
+import { Socket } from "socket.io";
+
+export const desconectar = (cliente: Socket)=>{
+    cliente.on('disconnect',()=>{
+       console.log('Cliente Desconectado'); 
+    });
+
+}
+// Escuchar mensajes
+export const mensaje = (cliente: Socket, io:SocketIO.Server) =>{
+cliente.on('mensaje',(payload: {de:string,cuerpo:string}) =>{
+console.log('Mensaje recibido', payload);
+io.emit('mensaje-nuevo',payload);
+
+});
+}
